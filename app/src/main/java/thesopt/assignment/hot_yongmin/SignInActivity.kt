089abfbe5e1ac66f818c9986ffadb1b0327a0613
권용민 <briandr@naver.com>
2022-04-05 14:38:18 +0900

    [FEAT] SignIn, SignUp logic complete

--- app/src/main/java/thesopt/assignment/hot_yongmin/SignInActivity.kt
+++ app/src/main/java/thesopt/assignment/hot_yongmin/SignInActivity.kt
@@ -1,11 +1,40 @@
 package thesopt.assignment.hot_yongmin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import thesopt.assignment.hot_yongmin.ContextExt.shortToast
+import thesopt.assignment.hot_yongmin.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val signUpIntent = Intent(this, SignUpActivity::class.java)
+        val homeIntent = Intent(this, HomeActivity::class.java)
+        val receiveIntent = intent
+
+        binding.btnSignup.setOnClickListener{
+            startActivity(signUpIntent)
+        }
+        binding.btnLogin.setOnClickListener{
+            if(binding.etId.text.isEmpty() or binding.etPw.text.isEmpty()){
+                this.shortToast("아이디/비밀번호를 확인해주세요")
+            }
+            else{
+                startActivity(homeIntent)
+                this.shortToast("로그인 성공")
+            }
+        }
+
+        binding.etId.setText(receiveIntent.getStringExtra("id"))
+        binding.etPw.setText(receiveIntent.getStringExtra("pw"))
     }
 }
--- app/src/main/java/thesopt/assignment/hot_yongmin/SignUpActivity.kt
+++ app/src/main/java/thesopt/assignment/hot_yongmin/SignUpActivity.kt
@@ -1,11 +1,30 @@
 package thesopt.assignment.hot_yongmin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import thesopt.assignment.hot_yongmin.ContextExt.shortToast
+import thesopt.assignment.hot_yongmin.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = Intent(this, SignInActivity::class.java)
+        binding.btnLogin.setOnClickListener{
+            if(binding.etId.text.isEmpty() or binding.etName.text.isEmpty() or binding.etPw.text.isEmpty()){
+                this.shortToast("입력되지 않은 정보가 있습니다")
+            }
+            else{
+                intent.putExtra("id", binding.etId.text.toString())
+                intent.putExtra("pw", binding.etPw.text.toString())
+                startActivity(intent)
+                this.shortToast("회원가입 되었습니다.")
+                finish()
+            }
+        }
     }
 }
