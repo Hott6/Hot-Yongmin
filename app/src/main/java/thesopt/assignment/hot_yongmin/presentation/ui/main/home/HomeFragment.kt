ba01bf317cc2d5e02b2fd4e3fd40b9939ae82c10
권용민 <briandr@naver.com>
2022-05-05 23:49:53 +0900

    3주차 과제 - tablayout

--- app/src/main/java/thesopt/assignment/hot_yongmin/presentation/ui/main/home/HomeFragment.kt
+++ app/src/main/java/thesopt/assignment/hot_yongmin/presentation/ui/main/home/HomeFragment.kt
@@ -5,15 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import thesopt.assignment.hot_yongmin.R
+import com.google.android.material.tabs.TabLayoutMediator
+import thesopt.assignment.hot_yongmin.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    private var _binding : FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var tabViewPagerAdapter : TabVpAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding= FragmentHomeBinding.inflate(layoutInflater)
+        initAdapter()
+        initTabLayout()
+        return binding.root
+    }
+
+    private fun initAdapter(){
+        val fragmentsList = listOf(Sample1Fragment(), Sample2Fragment())
+        tabViewPagerAdapter = TabVpAdapter(this)
+        tabViewPagerAdapter.fragmentsList.addAll(fragmentsList)
+        binding.vpHome.adapter=tabViewPagerAdapter
+    }
+
+    private fun initTabLayout(){
+        val tabLabel = listOf("FOLLOWING", "FOLLOWER")
+        TabLayoutMediator(binding.layoutTab, binding.vpHome){
+            tab, position->tab.text = tabLabel[position]
+        }.attach()
     }
 }
---
+++ app/src/main/java/thesopt/assignment/hot_yongmin/presentation/ui/main/home/Sample1Fragment.kt
@@ -0,0 +1,19 @@
+package thesopt.assignment.hot_yongmin.presentation.ui.main.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import thesopt.assignment.hot_yongmin.R
+
+class Sample1Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sample1, container, false)
+    }
+}
